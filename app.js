ef6628c2eb430ce2d698866f3151170d526ba0ce
OKozmovskaia <olga.kozmovskaia@gmail.com>
2020-12-15 16:05:25 +0100

    Complete client-side

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
-const url = "https://davy-api.herokuapp.com/predict";
+const form = document.querySelector('.form');
+ 
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-let dataBody = {
-        "area": 10,
-        "property-type": "APARTMENT" ,
-        "rooms-number": 2,
-        "zip-code": 1000,
-        // "garden": true,
-        // "equipped-kitchen": true,
-        // "furnished": false,
-        // "terrace": true,
-        // "facades-number": 2
-      
+  const formEvent = event.target;
+  const url = "https://davy-api.herokuapp.com/predict";
+  const urlCORS = "https://cors-anywhere.herokuapp.com/";
+  
+
+  // get values from formData
+  let formData = new FormData(formEvent);
+
+  // delete the empty-string
+  for(let pair of formData.entries()){
+    if(pair[1] == ""){
+      formData.delete(pair[0]);
+    };
+  };
+
+  const plainFormData = Object.fromEntries(formData.entries());
+
+  // convert to int and boolean
+  function replacer(key, value) {
+    if (!isNaN(value)) {
+      return parseInt(value);
     }
 
-  fetch (url, {
-    method: "POST",
-    body: JSON.stringify(dataBody),
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8 "
+    if (value === "true") {
+      return Boolean(value);
     }
-  })
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(err => console.log(err));
+    return value;
+  }
+
+  const dataBodyFetch = JSON.stringify(plainFormData, replacer);
+
+  console.log(dataBodyFetch);
+
+  fetch (urlCORS + url, {
+        method: "POST",
+        body: dataBodyFetch,
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8 "
+        }
+      })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json.Predicted_price);
+        const price = Number(json.Predicted_price);
+        
+        const priceContainer = document.querySelector(".price-container");
+        priceContainer.classList.remove("hide");
+        priceContainer.innerHTML = `
+        <p>Approximate cost: ${price} EUR</p>
+        `;
+      })
+      .catch(err => console.log(err));
+  
+});
